b576c17106903ec5a6bedee409c79299ec5750c5
johnmboudreaux <jhnbdrx@gmail.com>
2017-12-18 09:22:51 -0700

    first commit

---
+++ knexfile.js
@@ -0,0 +1,36 @@
+module.exports = {
+
+  development: {
+    client: 'pg',
+    connection: 'postgress://localhost/byob',
+    migrations: {
+      directory: './db/migrations'
+    },
+    seeds: {
+      directory: './db/seeds/dev'
+    },
+    useNullAsDefault: true
+  },
+  test: {
+    client: 'pg',
+    connection: process.env.DATABASE_URL || 'postgres://localhost/byob_test',
+    migrations: {
+      directory: './db/migrations'
+    },
+    seeds: {
+      directory: './db/seeds/test'
+    },
+    useNullAsDefault: true
+  },
+  production: {
+    client: 'pg',
+    connection: process.env.DATABASE_URL + `?ssl=true`,
+    migrations: {
+      directory: './db/migrations'
+    },
+    seeds: {
+      directory: './db/seeds/production'
+    },
+    useNullAsDefault: true
+  }
+};
---
+++ server.js
@@ -0,0 +1,70 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
+
+const environment = process.env.NODE_ENV || 'development';
+const configuration = require('./knexfile')[environment];
+const database = require('knex')(configuration);
+const jwt = require('jsonwebtoken');
+
+require('dotenv').config();
+
+app.set('port', process.env.PORT || 3000);
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static(__dirname + '/public'));
+
+
+app.post('/api/v1/authenticate', (request, response) => {
+
+});
+
+app.get('/api/v1/owners', (request, response) => {
+  database('home_owner').select()
+    .then(owners => response.status(200).json(owners))
+    .catch(error => response.status(500).json({error: `internal server error ${error}`}));
+});
+
+app.get('/api/v1/owners/:id', (request, response) => {
+
+});
+
+app.get('/api/v1/homes', (request, response) => {
+
+});
+
+app.get('/api/v1/owners/:id/homes', (request, response) => {
+
+});
+
+
+app.post('/api/v1/owners', (request, response) => {
+
+});
+
+app.post('/api/v1/owners/:id/homes', (request, response) => {
+
+});
+
+app.put('/api/v1/owners/:id', (request, response) => {
+
+});
+
+app.put('/api/v1/homes/:id', (request, response) => {
+
+});
+
+app.delete('/api/v1/owners/:id', (request, response) => {
+
+});
+
+app.delete('/api/v1/homes/:id', (request, response) => {
+
+});
+
+app.listen(app.get('port'), () => {
+  console.log(`App is running on ${app.get('port')}.`);
+});
+
+module.exports = app;
