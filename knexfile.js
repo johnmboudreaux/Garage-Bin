29fbe823239fc284649d20af03b9ede94c72ecf2
johnmboudreaux <jhnbdrx@gmail.com>
2017-12-18 17:20:20 -0700

    deploying to heroku

--- knexfile.js
+++ knexfile.js
@@ -21,5 +21,16 @@ module.exports = {
       directory: './db/seeds/test'
     },
     useNullAsDefault: true
+  },
+  production: {
+    client: 'pg',
+    connection: process.env.DATABASE_URL + `?ssl=true`,
+    migrations: {
+      directory: './db/migrations'
+    },
+    seeds: {
+      directory: './db/seeds/production'
+    },
+    useNullAsDefault: true
   }
 };
